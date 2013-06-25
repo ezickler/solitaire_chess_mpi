09da6b969dc4cf632cb2d304d3eaf6e2513680f9
Enno <Enno@blub.fritz.box>
2013-06-25 21:58:11 +0200

    add main and askparam

---
+++ askparam.h
@@ -0,0 +1,7 @@
+#ifndef ASKPARAMS_H_
+#define ASKPARAMS_H_
+
+void AskParam(struct options* options, int argc, char** argv);
+
+
+#endif
---
+++ main.h
@@ -0,0 +1,7 @@
+
+
+struct options
+{
+	int     num;            /* Number of threads                              */
+	int     method;         /* Gauss Seidel or Jacobi method of iteration     */
+};

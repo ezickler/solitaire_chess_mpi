8ee3822568a666d10c5dc161bcb17eea78ca68c5
Enno <e.zickler@gmail.com>
2013-07-11 00:07:32 +0200

    Spielbrett Erzeugung verbessert

--- main.c
+++ main.c
@@ -8,21 +8,28 @@
  * Erzeugt alle möglichen Spielbretter mit bis zu 10 Spielfiguren
  *
  */
-
 long long *erzeugeSpielbretter(){
 
 	long long *spielbretter;
 
+	int *spielbrett;
 
-	// Anzhl der Felder über die Iteriert werden muss
+	int anzFiguren;
+
+
+	// Anzahl der Felder, über die iteriert werden muss
 	int anzFelder = SpielbrettBreite * SpielbrettHoehe;
 
-	// Iteration für die Dame über alle Felder und eine zusätzlicher durchlauf für keine Dame
+	// Iteration für die Dame über alle Felder und ein zusätzlicher Durchlauf für keine Dame
+	// für alle anderen Figuren analog
 	for(int dame=0; dame<=anzFelder; dame++){
-		int *spielbrett;
+		anzFiguren=10;
 		// Im Letzten Iterationsschtritt keine Dame setzen
 		if(dame<anzFelder && spielbrett[dame]==0){
 			//TODO Dame auf Positon dame setzen z.B spielbrett[dame] = 6
+		}
+		else{
+			anzFiguren--;
 		}
 
 		// Iteration für den König
@@ -30,50 +37,79 @@ long long *erzeugeSpielbretter(){
 			if(koenig<anzFelder && spielbrett[koenig]==0){
 				//TODO König auf Positon koenig setzen z.B spielbrett[koenig] = 5
 			}
-
+			else{
+				anzFiguren--;
+			}
 			// Iteration für den Springer
 			for(int springer1=0; springer1<=anzFelder; springer1++){
 				if(springer1<anzFelder && spielbrett[springer1]==0){
 					//TODO Springer1 auf Positon springer1 setzen z.B spielbrett[springer1] = 4
 				}
-				// Iteration für den Springer
-				for(int springer2=0; springer2<=anzFelder; springer2++){
+				else{
+					anzFiguren--;
+				}
+				// Iteration für den Springer2
+				// geänderte Startposition, um Duplikate zu vermeiden
+				// analog für andere doppelte Figuren
+				for(int springer2=springer1+1; springer2<=anzFelder; springer2++){
 					if(springer1<anzFelder && spielbrett[springer1]==0){
 						//TODO Springer2 auf Positon springe2 setzen z.B spielbrett[springer2] = 4
+					}
+					else{
+						anzFiguren--;
 					}
 
 					for(int laeufer1=0; laeufer1<=anzFelder; laeufer1++){
 						if(laeufer1<anzFelder && spielbrett[laeufer1]==0){
 							//TODO Läufer1 auf Positon laeufer1 setzen z.B spielbrett[laeufer1] = 3
 						}
+						else{
+							anzFiguren--;
+						}
 
-						for(int laeufer2=0; laeufer2<=anzFelder; laeufer2++){
+						for(int laeufer2=laeufer1+1; laeufer2<=anzFelder; laeufer2++){
 							if(laeufer2<anzFelder && spielbrett[laeufer2]==0){
 								//TODO Läufer2 auf Positon laeufer2 setzen z.B spielbrett[laeufer2] = 3
+							}
+							else{
+								anzFiguren--;
 							}
 
 							for(int turm1=0; turm1<=anzFelder; turm1++){
 								if(turm1<anzFelder && spielbrett[turm1]==0){
 									//TODO Läufer2 auf Positon turm1 setzen z.B spielbrett[turm1] = 2
 								}
+								else{
+									anzFiguren--;
+								}
 
-								for(int turm2=0; turm2<=anzFelder; turm2++){
+								for(int turm2=turm1+1; turm2<=anzFelder; turm2++){
 									if(turm2<anzFelder && spielbrett[turm2]==0){
 										//TODO Turm2 auf Positon turm2 setzen z.B spielbrett[turm2] = 2
 									}
+									else{
+										anzFiguren--;
+									}
 
 									for(int bauer1=0; bauer1<=anzFelder; bauer1++){
-										if(bauer1<anzFelder){
+										if(bauer1<anzFelder && spielbrett[bauer1]==0){
 											//TODO Bauer1 auf Positon bauer1 setzen z.B spielbrett[bauer1] = 1
 										}
+										else{
+											anzFiguren--;
+										}
 
-										for(int bauer2=0; bauer2<=anzFelder; bauer2++){
-											if(bauer2<anzFelder){
+										for(int bauer2=bauer1+1; bauer2<=anzFelder; bauer2++){
+											if(bauer2<anzFelder  && spielbrett[bauer2]==0){
 												//TODO Bauer2 auf Positon bauer2 setzen z.B spielbrett[bauer2] = 1
 											}
+											else{
+												anzFiguren--;
+											}
 
-
-											//TODO Füge spielbrett zu spielbretter hinzu.
+											if (anzFiguren > 1){
+												//TODO Füge spielbrett zu spielbretter hinzu.
+											}
 
 										}
 									}
@@ -108,3 +144,8 @@ int main (int argc, char ** argv){
 	return EXIT_SUCCESS;
 	
 }
+
+
+
+
+
